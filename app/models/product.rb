658401a1ee314df6254295eb39c363c488289e8a
Zack Fisch <zack@zfisch.com>
2014-06-25 20:55:58 -0400

    Add associations between models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+	has_many: :reviews
+	has_many: :users, :through => :reviews
+
 	validates :description, :name, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true}
 
