f9b7cd8bd60c59dc14b08f18f1e0a8ed2b564d1c
Zack Fisch <zack@zfisch.com>
2014-06-25 21:30:44 -0400

    Writing the review routes, controllers and views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+
+	before_filer :load_product
+
+  def show
+  	@review = Review.find(params[:id])
+  end
+
+  def create
+  	@review = @product.reviews.build(review_params)
+  	# 	Same thing as @review = Review.new (
+  	#   :comment    => params[:review][:comment], 
+    #   :product_id => @product.id, 
+    #   :user_id    => current_user.id)
+  	@review.user_id = current_user.id
+
+  	if @review.save
+  		redirect_to products_path, notice: "Review created succesfully"
+  	else
+  		render 'products/show'
+  	end
+  end
+
+  def destroy
+  	@review = Review.find(params[:id])
+  	@review.destroy
+  end
+  
+  private
+  def review_params
+  	params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+  	@product = Product.find(params[:product_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resource :sessions, :only => [:new, :create, :destroy]
-
-  resources :users, :only => [:new, :create]
-
-  resources :products
+  
+  resources :products do 
+  	resources :reviews, :only => [:show, :create, :destroy]
+  end
 
 end
+
+# resources :sessions, :only => [:new, :create, :destroy]
+
+#   resources :users, :only => [:new, :create]
+
+#   resources :products
